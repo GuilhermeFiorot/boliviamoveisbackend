abf4c526054601211d0835b2498c48e5335b493b
laraguilar <lara.aguilar2003@gmail.com>
2024-06-22 16:16:21 -0300

    add: schemas validation

---
+++ app/schemas/product.py
@@ -0,0 +1,8 @@
+from marshmallow import Schema, fields
+
+class ProductSchema(Schema):
+    id = fields.Int(dump_only=True)
+    name = fields.Str(required=True)
+    description = fields.Str(required=True)
+    image = fields.Str()
+    price = fields.Float(required=True)
