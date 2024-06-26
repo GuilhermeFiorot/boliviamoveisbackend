d19c0576e0d9ddf553bbe0849be2fbf6893f0d2d
guilherme.fiorot <guilhermefirme3@gmail.com>
2024-06-26 08:21:23 -0300

    fix: nova api

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from .core.config import Config
 from .core.database import db, migrate
 from .api import api_blueprint
 from flask_jwt_extended import JWTManager
+from flask_cors import CORS
 
 def create_app():
     app = Flask(__name__)
@@ -12,7 +13,8 @@ def create_app():
     migrate.init_app(app, db)
 
     JWTManager(app)
-
+    CORS(app)
+    
     app.register_blueprint(api_blueprint, url_prefix='/api')
 
     return app
--- app/api/__init__.py
+++ app/api/__init__.py
@@ -4,6 +4,7 @@ from .product import ProductListResource, ProductResource
 from .user import UserListResource, UserResource
 from .auth import LoginResource, RegisterResource
 from .cart import CartResource
+from .forgot_password import ForgotPasswordResource
 
 api_blueprint = Blueprint('api', __name__)
 api = Api(api_blueprint)
@@ -16,5 +17,6 @@ api.add_resource(UserResource, '/v1/users/<int:id>')
 
 api.add_resource(LoginResource, '/v1/login')
 api.add_resource(RegisterResource, '/v1/register')
+api.add_resource(ForgotPasswordResource, '/v1/forgot_password')
 
 api.add_resource(CartResource, '/v1/cart')
